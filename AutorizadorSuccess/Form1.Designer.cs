31749034e4a776b9f06fc3bd4f341fc95405004e
Leonardo Chuello <ljchuello@gmail.com>
2021-06-25 10:30:22 -0500

    .

--- AutorizadorSuccess/Form1.Designer.cs
+++ AutorizadorSuccess/Form1.Designer.cs
@@ -29,13 +29,93 @@ namespace AutorizadorSuccess
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnProduccion = new MetroFramework.Controls.MetroButton();
+            this.btnPrueba = new MetroFramework.Controls.MetroButton();
+            this.btnLocal = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(24, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(307, 339);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(337, 64);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(307, 339);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(650, 64);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(307, 339);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // btnProduccion
+            // 
+            this.btnProduccion.Location = new System.Drawing.Point(887, 35);
+            this.btnProduccion.Name = "btnProduccion";
+            this.btnProduccion.Size = new System.Drawing.Size(75, 23);
+            this.btnProduccion.TabIndex = 2;
+            this.btnProduccion.Text = "Producción";
+            this.btnProduccion.UseSelectable = true;
+            // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(806, 35);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(75, 23);
+            this.btnPrueba.TabIndex = 3;
+            this.btnPrueba.Text = "Prueba";
+            this.btnPrueba.UseSelectable = true;
+            // 
+            // btnLocal
+            // 
+            this.btnLocal.Location = new System.Drawing.Point(725, 35);
+            this.btnLocal.Name = "btnLocal";
+            this.btnLocal.Size = new System.Drawing.Size(75, 23);
+            this.btnLocal.TabIndex = 4;
+            this.btnLocal.Text = "Local";
+            this.btnLocal.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(985, 426);
+            this.Controls.Add(this.btnLocal);
+            this.Controls.Add(this.btnPrueba);
+            this.Controls.Add(this.btnProduccion);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private MetroFramework.Controls.MetroButton btnProduccion;
+        private MetroFramework.Controls.MetroButton btnPrueba;
+        private MetroFramework.Controls.MetroButton btnLocal;
     }
 }
 
--- AutorizadorSuccess/Form1.cs
+++ AutorizadorSuccess/Form1.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using MetroFramework.Forms;
 
 namespace AutorizadorSuccess
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
+        private bool activo = false;
+
         public Form1()
         {
             InitializeComponent();
+            Text = "Mile Autorizador 🤣";
         }
     }
 }
